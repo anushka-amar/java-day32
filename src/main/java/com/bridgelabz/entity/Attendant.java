be8fefe726113f5371c5c6e6b0170bec4824df1d
Anushka <anushkamar1102@gmail.com>
2023-12-29 21:47:08 +0530

    Added: attendant class to manage attendants

---
+++ src/main/java/com/bridgelabz/entity/Attendant.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.entity;
+
+/* attendant class to initialise attendant for UC6 */
+public class Attendant {
+    private String attId;
+
+    public Attendant(String attId){
+        this.attId = attId;
+    }
+
+    public String getAttId(){
+        return attId;
+    }
+}
