da822bb0ff88d0747e9337aaf7a9c81cd623f206
Anushka <anushkamar1102@gmail.com>
2023-12-29 22:02:04 +0530

    Added: TC-7 find car in parking lot

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -15,6 +15,7 @@ class ParkingLotTest {
         parkingLot = new ParkingLot(2);
     }
 
+    //UC1
     @Test
     void givenCarId_carShouldBeParkedSuccessfully(){
         Car car1 = new Car("CR-ID-001", "Blue");
@@ -22,6 +23,7 @@ class ParkingLotTest {
         assertTrue(parkingLot.check_parked("CR-ID-001"));
     }
 
+    //UC2
     @Test
     void givenCarId_carShouldBeUnParkedSuccessfully(){
         parkingLot.parkCar(new Car("CR-ID-001", "Blue"));
@@ -29,6 +31,7 @@ class ParkingLotTest {
         assertFalse(parkingLot.check_parked("CR-ID-001"));
     }
 
+    //UC4
     @Test
     void ifParkingLotFull_notifiesAirportSecurity(){
         parkingLot.parkCar(new Car("CR-ID-001", "Blue"));
@@ -36,6 +39,7 @@ class ParkingLotTest {
         assertEquals("Please redirect Security staffs", parkingLot.check_parking_status());
     }
 
+    //UC5
     @Test
     void ifParkingLotHasSpace_notifyOwnerTORemoveFullSign(){
         parkingLot.parkCar(new Car("CR-ID-001", "Blue"));
@@ -46,9 +50,17 @@ class ParkingLotTest {
         assertEquals("Slots are available in the Parking lot.", parkingLot.check_parking_status());
     }
 
+    //UC6
     @Test
     void givenCarIdAndAttendant_carShouldBeParkedSuccessfully(){
         parkingLot.parkCar(new Car("CR-ID-001", "Blue"), new Attendant("Att-001"));
         assertTrue(parkingLot.parkCar(new Car("CR-ID-001", "Blue"),new Attendant("Att-001")));
+    }
+
+    //UC7
+    @Test
+    void givenCarId_ifExists_shouldReturnTrue(){
+        parkingLot.parkCar(new Car("CR-ID-001", "Blue"));
+        assertTrue(parkingLot.check_parked("CR-ID-001"));
     }
 }
