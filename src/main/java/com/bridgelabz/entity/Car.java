19ecd4eea354ddec10c2ec0efed3cb254fdd1800
Anushka <anushkamar1102@gmail.com>
2023-12-31 11:31:23 +0530

    Added: field to add time stamp when car is parked

--- src/main/java/com/bridgelabz/entity/Car.java
+++ src/main/java/com/bridgelabz/entity/Car.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.entity;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /* manages the property of each car in the list */
 public class Car {
     private String carId;
     private String color;
 
+    private LocalDateTime parked_time;
+
     public Car(String carId, String color) {
         this.carId = carId;
         this.color = color;
+        this.parked_time = LocalDateTime.now(); //updates the time for parked car
     }
 
     public String getCarId() {
@@ -16,5 +23,14 @@ public class Car {
 
     public String getColor() {
         return color;
+    }
+
+    public LocalDateTime getParked_time() {
+        return parked_time;
+    }
+
+    public String getFormattedParkedTime(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return parked_time.format(formatter);
     }
 }
